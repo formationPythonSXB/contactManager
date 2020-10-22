218cd7fa3be0f128c58808e0bba9b348e8a0dd41
Alexandre Aubin <alex.aubin@mailoo.org>
2020-10-22 23:38:21 +0200

    Testing swagger documentation using flasgger

--- app.py
+++ app.py
@@ -8,9 +8,28 @@ def homepage():
     return render_template('homepage.html', contacts=contacts)
 
 
-
 @app.route('/add', methods=['POST'])
 def add_contact():
+    """
+    Endpoint to add a contact
+    ---
+    parameters:
+      - name: name
+        in: body
+        type: string
+        required: true
+      - name: phone
+        in: body
+        type: string
+        required: true
+      - name: email
+        in: body
+        type: string
+        required: true
+    responses:
+      200:
+        description: Successfully created the contact
+    """
     db.session.add(Contact(name=request.form["name"],
                            phone=request.form["phone"],
                            email=request.form["email"],
--- common.py
+++ common.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+swagger = Swagger(app)
