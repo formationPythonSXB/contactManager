bf4c72f864a86bdee76e7aeee4c9be8544379ca8
Alexandre Aubin <alex.aubin@mailoo.org>
2020-10-22 23:19:28 +0200

    Test to reimplement pieces of API using flask-restful

--- initialize.py
+++ initialize.py
@@ -1,11 +1,12 @@
 from common import db
 from models import Contact
+import datetime
 
 db.drop_all()
 db.create_all()
 
 db.session.add(Contact(name="Alice",   phone="1111", email='alice@gmail.com', birthday=None))
 db.session.add(Contact(name="Bob",     phone="2222", email='bob@yahoo.fr', birthday=None))
-db.session.add(Contact(name="Camille", phone="3333", email='camille@hotmail.com', birthday="?"))
+db.session.add(Contact(name="Camille", phone="3333", email='camille@hotmail.com', birthday=datetime.datetime.now()))
 
 db.session.commit()
